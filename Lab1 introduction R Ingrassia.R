e77ecc40ab3e5fbce17c4a0abee60a3badbc70ad
Lingrass424 <49769563+Lingrass424@users.noreply.github.com>
2019-09-22 11:43:44 -0400

    Add files via upload

---
+++ Lab1 introduction R Ingrassia.R
@@ -0,0 +1,17 @@
+install.packages("tidyverse")
+library(tidyverse)
+my_num <- 2/2
+my_char <-"hi!"
+my_bool <- TRUE
+typof(my_num)
+typeof(my_char)
+typeof(my_bool)
+
+my_nan <- 0/0
+my_nan
+typeof(my_nan)
+
+#creates a cnull variable
+my_nan <- 0/0
+
+help(type)
